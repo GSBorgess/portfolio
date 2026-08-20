fee87b19ef220bb6bfcb3e18b629e8242dadb951
Gabriel <gabrielborgesschmidt@gmail.com>
2026-08-20 06:47:00 -0300

    feature: adiciona efeito scroll

---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,23 @@
+import { ReactLenis } from "lenis/react";
+import "lenis/dist/lenis.css";
+import type { ReactNode } from "react";
+
+type SmoothScrollProps = {
+  children: ReactNode;
+};
+
+export function SmoothScroll({ children }: SmoothScrollProps) {
+  return (
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.08,
+        smoothWheel: true,
+        wheelMultiplier: 0.9,
+        touchMultiplier: 1.2,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  );
+}
